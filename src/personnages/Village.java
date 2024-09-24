2b42ba7af0dc7718f85ad2a341ab5744badd89c1
Ludovic Racot <rcl5080a@ensfsi.onmicrosoft.com>
2024-09-24 19:57:15 +0200

    fin séance TP2

--- src/personnages/Village.java
+++ src/personnages/Village.java
@@ -2,12 +2,31 @@ package personnages;
 
 public class Village {
 	private String nom;
-
-	public Village(String nom) {
+	private Chef chef;
+	private Gaulois[] villageois;
+	private int nbVillageois = 0;
+	
+	public Village(String nom, int nbVillageoisMaximum) {
 		this.nom = nom;
+		this.villageois = new Gaulois[nbVillageoisMaximum];
+	}
+	
+	public void setChef(Chef chef) {
+		this.chef = chef;
+	}
+	
+	public void ajouterHabitant(Gaulois villageois) {
+		this.villageois[nbVillageois] = villageois;
+		nbVillageois++;
+	}
+	
+	public Gaulois trouverHabitant(int numeroVillageois) {
+		return villageois[numeroVillageois];
 	}
 
 	public String getNom() {
 		return nom;
 	}
+	
+	
 }
